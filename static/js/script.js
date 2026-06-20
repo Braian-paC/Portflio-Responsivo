36f1c56049d99c45bab43a90182f0dc8ea979525
Braian-paC <braianbbg4@gmail.com>
2026-06-20 20:14:23 -0300

    Adjusting elements in home HTML/CSS

--- static/js/script.js
+++ static/js/script.js
@@ -5,12 +5,8 @@ document.getElementById("arrow_button_home_up").addEventListener("click", () => 
     scrollUp()
 })
 
-document.getElementById("nobl_button").addEventListener("click", () => {
-    window.open('https://nobl.io', '_blank');
-})
-
 function scrollDown() {
-    window.scrollTo(0, 750)
+    window.scrollTo(0, 730)
 }
 function scrollUp() {
     window.scrollTo(0, 0)
